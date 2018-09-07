c87d37abd4fa22473b43d749ae980eaa2f8782f9
Tretyakov Dmitry <tretyakov.dmi@gmail.com>
2018-09-07 09:55:06 +0300

    docker, postgres, models

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const { ApolloServer } = require('apollo-server-express');
+import { ApolloServer } from 'apollo-server-express';
 
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
+import models from './models';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+const greetings = () => console.log(`
+🚀 Server ready at http://localhost:4000${server.graphqlPath}
+`)
+
+models.sequelize.sync({ force: true }).then(() => {
+    app.listen({ port: 4000 }, greetings)
+})
+
